33cea8cd596cc525e8d235f93995d83368d69455
faizansk <faizansk814@gmail.com>
2023-09-07 14:10:35 +0530

    cart and auth added

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -8,6 +8,8 @@ const db_1 = require("./connection/db");
 const user_routes_1 = __importDefault(require("./routes/user.routes"));
 const cors_1 = __importDefault(require("cors"));
 const product_routes_1 = __importDefault(require("./routes/product.routes"));
+const auth_1 = require("./middlewares/auth");
+const cart_router_1 = __importDefault(require("./routes/cart.router"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
@@ -16,6 +18,8 @@ app.get("/", (req, res) => {
 });
 app.use("/user", user_routes_1.default);
 app.use("/product", product_routes_1.default);
+app.use(auth_1.auth);
+app.use("/cart", cart_router_1.default);
 app.listen(4031, async () => {
     try {
         await db_1.connection;
---
+++ backend/src/models/cart.model.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface Cart extends Document{
+    userid:mongoose.Schema.Types.ObjectId,
+    productid:mongoose.Schema.Types.ObjectId
+    quantity:number
+}
+
+const CartSchema:Schema=new Schema({
+    userid:{type:mongoose.Schema.Types.ObjectId,ref:"user"},
+    productid:{type:mongoose.Schema.Types.ObjectId,ref:"product"},
+    quantity:{type:Number,default:1}
+})
+
+export let CartModel=mongoose.model<Cart>("cart",CartSchema)
